b1a73439e71054156493754d019c138a4daef125
Abhishek Kumar <abhishekshanu619@gmail.com>
2021-09-26 20:38:30 +0530

    deploy app config

---
+++ app.js
@@ -0,0 +1,88 @@
+require("dotenv").config(); 
+const express = require('express')
+const bodyParser= require('body-parser');
+const cors=require('cors');
+const mongoose = require("mongoose");
+const { response } = require("express");
+
+const app= express()
+const port = process.env.PORT || 3001
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+const toDoListSchema = new mongoose.Schema({
+    item_:String
+});
+const item = mongoose.model('item', toDoListSchema);
+
+app.get('/',(req,res)=>{
+    item.find({},(found,err)=>{
+        if(!err){
+            // console.log(found)
+            res.send(found)
+        }
+        // console.log(err);
+        res.send(err);
+    })
+})
+
+app.post('/',(req,res)=>{
+    const toDoIte = new item({
+        item_:req.body.toDoContent
+    });
+    toDoIte.save()
+    .then((response) =>res.send(response))
+    .catch((errors)=>{
+        console.log("Problem in saving in the database")
+        res.send("Not connected to the database")
+    })
+    // console.log(req.body)
+})
+
+app.delete('/',(req,res)=>{
+    if(!Object.keys(req.body).length){
+        item.deleteMany({})
+        .then((response)=>{
+            res.send("data deleted")
+        })
+        .catch((err)=>{
+            res.send(err)
+        })
+    }
+    else{
+        item.deleteOne({_id:req.body.id})
+        .then((response)=>{
+            console.log("delete one response to client",response)
+            res.send(response)
+        })
+        .catch((err)=>res.send(err))
+        // console.log(req.body.id)
+    }
+})
+
+//heroko deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static("client/build"));
+}
+
+app.listen(port,()=>{
+    console.log(`App is listening on port no. http://localhost:${port}`);
+})
+
