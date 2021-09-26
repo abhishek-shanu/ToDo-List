89c00f9e9fb605ef12b31c558828d6e5866798d0
Abhishek Kumar <abhishekshanu619@gmail.com>
2021-09-27 00:37:18 +0530

    deployment rel-changes in app.js

--- app.js
+++ app.js
@@ -78,8 +78,12 @@ app.delete('/',(req,res)=>{
 })
 
 //heroko deployment
-if(process.env.NODE_ENV==="production"){
+if ( process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 app.listen(port,()=>{
